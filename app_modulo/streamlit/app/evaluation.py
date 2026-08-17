c8efcceed76e6adaec84a7ce839cbcf78042f251
lucasfellipe-datauniverse <lucasfellipe8888@gmail.com>
2026-08-17 16:34:35 -0300

    modulo avaliacao rag e dados historicos para avaliacao

---
+++ app_modulo/streamlit/app/evaluation.py
@@ -0,0 +1,45 @@
+#%%
+import pandas as pd
+
+#%%
+def hit_rate(relevance_total):
+    # calcula o hit rate
+    cnt = 0
+    for line in relevance_total:
+        if True in line:
+            cnt += 1
+ 
+    return cnt / len(relevance_total)            
+
+def mrr(relevance_total):
+    # calcula o mean reciprocal rank
+    score = 0
+
+    for line in relevance_total:
+        for rank in line:
+            if line[rank] == True:
+                score += 1 / (rank + 1) 
+            break
+
+    return score / len(relevance_total)
+
+def evaluate(research_funtion):
+    # verifica se o rag buscou informacao correta ou incorreta para a requisicao
+    ground_truth = pd.read_csv('streamlit/app/dadosHistoricos/dataset.csv') 
+    ground_truth.to_dict(orient='records')    
+
+    relevance_total = []
+
+    for line in ground_truth:
+        doc_id = line['documnet']
+
+        results = research_funtion(line) # research_fuc fara..
+
+        relevance = [doc_id==line_results['doc_id'] for line_results in results]
+
+        relevance_total.append(relevance)
+
+    return {
+        'hit_rate': hit_rate(relevance_total),
+        'mrr': mrr(relevance_total)
+    }    
